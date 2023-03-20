1530e316ccd3be3f53720629435e5254606fdcaf
Sai Santosh Pal <78339151+Sai-Santosh-Pal@users.noreply.github.com>
2023-03-20 22:15:37 +0530

    Update execute.py

--- execute.py
+++ execute.py
@@ -8,17 +8,7 @@ pip.main(['install', 'pytesseract'])
 pip.main(['install', 'zipfile'])
 pip.main(['install', 'fitz'])
 pip.main(['install', 'googletrans'])
-print("Done!!!")
 
-from zipfile import ZipFile
-  
-# loading the temp.zip and creating a zip object
-with ZipFile(".\package.zip", 'r') as zObject:
-  
-    # Extracting all the members of the zip 
-    # into a specific location.
-    zObject.extractall(
-        path=".\\")
 import os
 os.chdir("./v0.2/")
 os.system("python main.py")
