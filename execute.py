45ae45bf46a474592223f172fac0d7d4eb2ddb27
Sai Santosh Pal <78339151+Sai-Santosh-Pal@users.noreply.github.com>
2023-03-20 18:56:38 +0530

    Create execute.py

---
+++ execute.py
@@ -0,0 +1,23 @@
+import pip
+pip.main(['install', 'tkinter'])
+# PyPdf2 pathlib Pillow pytesseract PIL fitz googletrans
+pip.main(['install', 'PyPdf2'])
+pip.main(['install', 'pathlib'])
+pip.main(['install', 'Pillow'])
+pip.main(['install', 'pytesseract'])
+pip.main(['install', 'zipfile'])
+pip.main(['install', 'fitz'])
+pip.main(['install', 'googletrans'])
+print("Done!!!")
+
+from zipfile import ZipFile
+  
+# loading the temp.zip and creating a zip object
+with ZipFile(".\package.zip", 'r') as zObject:
+  
+    # Extracting all the members of the zip 
+    # into a specific location.
+    zObject.extractall(
+        path=".\\")
+import os
+os.system("python main.py")
